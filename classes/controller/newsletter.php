ca5c0e18b38c773aba109d09b554498e98c4c097
jakimfett <jakimfett@gmail.com>
2015-09-08 19:28:23 -0700

    Move email methods to model

--- classes/controller/newsletter.php
+++ classes/controller/newsletter.php
@@ -14,8 +14,8 @@ class Controller_Newsletter extends Controller
     public function action_subscribe()
     {
         $email_address = $this->request->post('email');
-        $name          = $this->request->post('name');
-        $ip_raw        = $this->request->post('ip');
+        $name = $this->request->post('name');
+        $ip_raw = $this->request->post('ip');
 
         if (isset($email_address)) {
 
@@ -23,7 +23,7 @@ class Controller_Newsletter extends Controller
             if ($duplicate_check->count() !== 0) {
                 die('DUPLICATE');
             } else {
-                $city   = null;
+                $city = null;
                 $region = null;
                 if (!filter_var($ip_raw, FILTER_VALIDATE_IP) === false) {
 
@@ -47,8 +47,7 @@ class Controller_Newsletter extends Controller
 
                 if (isset($insert[0])) {
                     if ($insert[0] > 0) {
-                        if ($this->_send_confirmation_email($email_address, $name)) {
-                            $this->_send_admin_confirmation_email($email_address, $name);
+                        if ($this->_model->send_email_confirmations($email_address, $name)) {
                             die('SUBSCRIBED');
                         }
                     }
@@ -84,45 +83,9 @@ class Controller_Newsletter extends Controller
         }
     }
 
-    private function _send_confirmation_email($email_address, $name = NULL)
-    {
-        $email               = Email::factory();
-        $email->subject("Newsletter Subscription Confirmation");
-        $email->to($email_address);
-        $email->from("noreply@solvethelabyrinth.com", "Labyrinth");
-        $view                = View::factory('email/newsletter-subscribe-confirmation');
-        $view->email_address = $email_address;
-
-        if (isset($name)) {
-            $view->name = $name;
-        }
-        
-        $template = 'pages/newsletter-subscribe-confirm';
-        
-        $view->text = Post::dcache(Path::lookup($template)['id'], 'page', Config::load('pages'))->body;
-        
-        $email->message($view, "text/html");
-        return $email->send();
-    }
-
-    private function _send_admin_confirmation_email($email_address, $name = NULL)
-    {
-        $admin_email = Email::factory();
-
-        $admin_email->subject("Newsletter Subscription Confirmation");
-        $admin_email->to("webmaster@solvethelabyrinth.com");
-        $admin_email->from("noreply@solvethelabyrinth.com", "Labyrinth");
-        $message = "Newsletter subscriber<br/>email: " . $email_address;
-        if (isset($name)) {
-            $message .= "<br/>name: " . $name;
-        }
-        $admin_email->message($message, "text/html");
-        return $admin_email->send();
-    }
-
     public function action_list()
     {
-        $view              = View::factory('admin/newsletter-subscribers-list');
+        $view = View::factory('admin/newsletter-subscribers-list');
         $view->subscribers = $this->_model->get_all_subscribers();
         $this->response->body($view);
     }
--- classes/model/newsletter.php
+++ classes/model/newsletter.php
@@ -8,7 +8,7 @@ class Model_Newsletter extends Model
 
     /**
      * Subscribe to the newsletter, given an email address and several optional params
-     * 
+     *
      * @param string $email_address email address, required
      * @param string $name subscriber's name, if given
      * @param int $ip IP address
@@ -23,7 +23,7 @@ class Model_Newsletter extends Model
 
     /**
      * Check whether an email address is already in the database
-     * 
+     *
      * @param string $email_address
      * @return Database result object
      */
@@ -33,8 +33,61 @@ class Model_Newsletter extends Model
     }
 
     /**
-     * Add a comment for a specific email address
+     * Send email confirmations to user and admin
      * 
+     * @param type $email_address
+     * @param type $name
+     * @return boolean
+     */
+    public function send_email_confirmations($email_address, $name = NULL)
+    {
+        if ($this->_send_confirmation_email($email_address, $name)) {
+            $this->_send_admin_confirmation_email($email_address, $name);
+            return TRUE;
+        }
+
+        return FALSE;
+    }
+
+    private function _send_confirmation_email($email_address, $name = NULL)
+    {
+        $email               = Email::factory();
+        $email->subject("Newsletter Subscription Confirmation");
+        $email->to($email_address);
+        $email->from("noreply@solvethelabyrinth.com", "Labyrinth");
+        $view                = View::factory('email/newsletter-subscribe-confirmation');
+        $view->email_address = $email_address;
+
+        if (isset($name)) {
+            $view->name = $name;
+        }
+
+        $template = 'pages/newsletter-subscribe-confirm';
+
+        $view->text = Post::dcache(Path::lookup($template)['id'], 'page', Config::load('pages'))->body;
+
+        $email->message($view, "text/html");
+        return $email->send();
+    }
+
+    private function _send_admin_confirmation_email($email_address, $name = NULL)
+    {
+        $admin_email = Email::factory();
+
+        $admin_email->subject("Newsletter Subscription Confirmation");
+        $admin_email->to("webmaster@solvethelabyrinth.com");
+        $admin_email->from("noreply@solvethelabyrinth.com", "Labyrinth");
+        $message = "Newsletter subscriber<br/>email: " . $email_address;
+        if (isset($name)) {
+            $message .= "<br/>name: " . $name;
+        }
+        $admin_email->message($message, "text/html");
+        return $admin_email->send();
+    }
+
+    /**
+     * Add a comment for a specific email address
+     *
      * @param string $email_address
      * @param string $comment
      * @return Database result object
@@ -55,7 +108,7 @@ class Model_Newsletter extends Model
 
     /**
      * Get a note for a given email address
-     * 
+     *
      * @param string $email_address
      * @return Database result object
      */
@@ -66,7 +119,7 @@ class Model_Newsletter extends Model
 
     /**
      * Set the opt-out flag for an email address
-     * 
+     *
      * @param string $email_address
      * @param boolean $boolean
      * @return Database result object
@@ -78,7 +131,7 @@ class Model_Newsletter extends Model
 
     /**
      * Get the opt-out flag for an email address
-     * 
+     *
      * @param string $email_address
      * @return Database result object
      */
@@ -89,7 +142,7 @@ class Model_Newsletter extends Model
 
     /**
      * Get all the subscribers, without filtering
-     * 
+     *
      * @return Database result object
      */
     public function get_all_subscribers()
